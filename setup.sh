dcf1c67621039b7d1276cb72d9cf86eddc4e32d4
Guilherme Donato <guilhermednt@users.noreply.github.com>
2018-08-17 11:50:06 -0300

    updated Artifactory URIs

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@
 # Setup Composer
 if [ -x "$(command -v composer)" ]; then
     echo "Configuring composer..."
-    composer config --global repositories.procergs_cache composer https://artifactory.des.intra.rs.gov.br/artifactory/api/composer/php-remote
-    composer config --global repositories.procergs_private composer https://artifactory.des.intra.rs.gov.br/artifactory/api/composer/php-local
+    composer config --global repositories.procergs_cache composer https://secweb.intra.rs.gov.br/artifactory/api/composer/php-remote
+    composer config --global repositories.procergs_private composer https://secweb.intra.rs.gov.br/artifactory/api/composer/php-local
     composer config --global repo.packagist false
 fi
 
 # Setup NPM
 if [ -x "$(command -v npm)" ]; then
     echo "Configuring npm..."
-    npm config set registry https://artifactory.des.intra.rs.gov.br/artifactory/api/npm/npm/
+    npm config set registry https://secweb.intra.rs.gov.br/artifactory/api/npm/npm/
 fi
 
 # Setup NuGet
 if [ -x "$(command -v nuget)" ]; then
     echo "Configuring nuget..."
-    nuget sources Add -Name Artifactory -Source https://artifactory.des.intra.rs.gov.br/artifactory/api/nuget/nuget
+    nuget sources Add -Name Artifactory -Source https://secweb.intra.rs.gov.br/artifactory/api/nuget/nuget
 fi
 
 # Setup Maven
